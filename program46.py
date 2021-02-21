9e92a31a3e92792fcb2c043e198c5f5c09281c1d
ShohanurSourav <shohanur15-1965@diu.edu.bd>
2021-02-21 16:21:13 +0600

    program 46 and 47 are added

---
+++ program46.py
@@ -0,0 +1,16 @@
+# class and object
+
+class Student:
+    roll = ""
+    gpa = ""
+
+
+Sourav = Student()
+Sourav.roll = 1965
+Sourav.gpa = 3.81
+print(f"Sourav's --> Roll : {Sourav.roll}, GPA : {Sourav.gpa}")
+
+Sumaiya = Student()
+Sumaiya.roll = 1752
+Sumaiya.gpa = 3.71
+print(f"Sumaiya's --> Roll : {Sumaiya.roll}, GPA : {Sumaiya.gpa}")
