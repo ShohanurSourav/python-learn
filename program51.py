ce8ef0cf18379f6d36b7ef9bd9828615f89549a0
ShohanurSourav <shohanur15-1965@diu.edu.bd>
2021-02-25 14:49:43 +0600

    program 51 is added

---
+++ program51.py
@@ -0,0 +1,16 @@
+# method overloading
+
+class Phone:
+    def __init__(self):
+        print("I am in Phone class")
+
+
+class Samsung(Phone):
+    # phone class er init k overload korbe nicher init
+    def __init__(self):
+        # super().__init__()
+        print("I am in Samsung class")
+
+
+# samsung class er object
+s = Samsung()
