d611e7fd4a7815a047abcf2eb28c95f40c44cd58
S.M Shohanur Hossain Sourav <42938943+ShohanurSourav@users.noreply.github.com>
2021-02-25 15:11:59 +0600

    Update program52.py

--- program52.py
+++ program52.py
@@ -7,12 +7,14 @@ class Shape:
         print("I am area method of shape class")
 
 
+# triangle class inherits Shape class' property        
 class Triangle(Shape):
     def area(self):
         area = 0.5 * self.dim1 * self.dim2
         print("Area of Triangle : ", area)
+        
 
-
+# rectangle class inherits Shape class' property
 class Rectangle(Shape):
     def area(self):
         area = self.dim1 * self.dim2
