b312c924109cf961154abe76b9c3f2b814cb1cd5
S.M Shohanur Hossain Sourav <42938943+ShohanurSourav@users.noreply.github.com>
2021-02-26 19:39:20 +0600

    Update program56.py

--- program56.py
+++ program56.py
@@ -8,7 +8,8 @@ class Bike:
     # magic method to check the obj value r same or not
     def __eq__(self, other):
         return self.name == other.name and self.color == other.color
-
+    
+    # magic method to print the obj value
     def __str__(self):
         return (f"Name = {self.name}, Color = {self.color}")
 
