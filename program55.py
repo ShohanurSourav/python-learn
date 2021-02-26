885ebe6dacd9a72b8ef23cb09fa8c04cebb3a722
ShohanurSourav <shohanur15-1965@diu.edu.bd>
2021-02-26 14:53:34 +0600

    program 55 is added

---
+++ program55.py
@@ -0,0 +1,12 @@
+# built in polymorphic function
+print(len("Anisul Islam"))
+print(len([10, 20, 30]))
+
+
+# user defined polymorphic function
+def add(x, y, z=0):
+    return x + y + z
+
+
+print(add(2, 3))
+print(add(2, 3, 4))
