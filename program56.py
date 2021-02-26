1bf6d11bd7568f93be570de9be9b5c3d49faf9e9
ShohanurSourav <shohanur15-1965@diu.edu.bd>
2021-02-26 19:56:03 +0600

    program 57 module is added

--- program56.py
+++ program56.py
@@ -8,10 +8,10 @@ class Bike:
     # magic method to check the obj value r same or not
     def __eq__(self, other):
         return self.name == other.name and self.color == other.color
-    
+
     # magic method to print the obj value
     def __str__(self):
-        return f"Name = {self.name}, Color = {self.color}"
+        return (f"Name = {self.name}, Color = {self.color}")
 
     def display(self):
         print(f"Name = {self.name}, Color = {self.color}")
---
+++ program57.py
@@ -0,0 +1,5 @@
+# module
+
+from program49 import Triangle
+
+Triangle(20, 30)
